62a5c888e01b5610edcb6066bd6ff3fb03b4f83c
Ilia Yavorsky <ilia.bubna.yavorsky@gmail.com>
2017-08-21 19:41:48 +0300

    .sh for easier starting

---
+++ contacts_book_web/contacts_book_web/build_dockers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo mvn docker:build
+sudo docker network create --subnet=172.18.0.0/16 skynet
+sudo mvn docker:stop
+service postgresql stop
+sudo mvn docker:start
+OUTPUT="$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' bubna_docker_postgresql)"
+echo "ip of postgresql container is ${OUTPUT}"
+OUTPUT="$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' bubna_docker_tomcat)"
+echo "ip of tomcat container is ${OUTPUT}"
+echo "change pom.xml, context.xml, maven's settings.xml (i don't where is it on your pc, sorry)"
